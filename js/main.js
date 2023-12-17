884fcabf9df723a611604051fe1ae3546a7007eb
alru2002 <115723072+alru2002@users.noreply.github.com>
2023-12-17 22:35:53 +0100

    ya tamos

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function Desplegar() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
