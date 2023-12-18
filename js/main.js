4d34e2ad6b311761b1933a2f64b63d9d58b9de9a
alru2002 <115723072+alru2002@users.noreply.github.com>
2023-12-18 23:07:46 +0100

    Ya casi tamos

--- js/main.js
+++ js/main.js
@@ -6,3 +6,22 @@ function Desplegar() {
         x.className = "";
     }
 }
+
+window.onscroll = function(){
+    CargarBarras();
+
+}
+
+function CargarBarras(){
+    var hard = document.getElementById("hard")
+    var distancia = window.innerHeight - hard.getBoundingClientRect().top;
+    if (distancia >= 10){
+        document.getElementById("python").classList.add("p1");
+        document.getElementById("JAVA").classList.add("p2");
+        document.getElementById("C").classList.add("p3");
+        document.getElementById("C#").classList.add("p4");
+        document.getElementById("HTML").classList.add("p5");
+        document.getElementById("JS").classList.add("p6");
+    }
+
+}
